d38db5ea975d49a10ce53af86ff41132430f6cf5
brot-senpai <tran.788@osu.edu>
2020-11-21 15:31:45 -0500

    1st

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import logo from '../../img/bread.svg';
 
+const imgStyle = {
+    color: "#000000",
+    height: 600,
+    WebkitFilter: "invert(100%)",
+    filter: "invert(100%)"
+    
+}
+
 function Home(props) {
     return(
-        <div>
-            <img src={logo} alt="brot-senpai" height="600"/>
+        <div style={{backgroundColor: "#000000"}}>
+            <img src={logo} alt="brot-senpai" style={imgStyle}/>
         </div>
     )
 }
